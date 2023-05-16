b95f5d390141c02e23bcd33116a47cffd5d06cd2
Romulo <romulotg12@gmail.com>
2023-05-16 00:27:38 -0300

    cart, order and stripe tests

---
+++ api/routes/productRoute.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const ProductController = require("../controllers/ProductController")
+const { verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin} = require("./verifyToken");
+
+
+router.post("/add", verifyTokenAndAdmin, ProductController.createProduct)
+
+router.put("/change/:id", verifyTokenAndAdmin, ProductController.updateProduct)
+
+router.delete("/delete/:id", verifyTokenAndAdmin, ProductController.deleteProduct)
+
+router.get("/find/:id", ProductController.getProduct)
+
+router.get("/find-all", ProductController.getAllProducts)
+
+router.get("/", ProductController.getByCategories)
+
+
+module.exports = router;
